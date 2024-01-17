6090ff690c8b99a2013ea328afecacb107a672df
TsingLoo <xiaonan.pan19@student.xjtlu.edu.cn>
2024-01-18 01:32:21 +0800

    Add  video frame for EolaneVR

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -1,19 +1,46 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("project-1", "Optimistic Chubby", "img/projects/project-1-icon.png", 
+    new ProjectData("project-1", "EolaneVR", "https://images.tsingloo.com/ShareX/2024/01/chrome_c1onMoS0gY.gif", 
     `
-    <div class="paragraph">
-     <strong>Optimistic Chubby</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-     <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
-    </div>
-    <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div class="paragraph center">
-        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
-    </div>
+    <style>
+    body {
+        margin: 50;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    .video-container {
+        padding: 5px; /* Adjust padding as needed */
+        box-sizing: border-box;
+        width: 80%; /* Video container takes 80% of body width */
+        max-width: 1920px; /* Maximum width of the video container */
+    }
+    video {
+        width: 100%; /* Video takes full width of its container */
+        height: auto; /* Height is set automatically */
+    }
+    </style>
 
+    <div class="paragraph">
+     <strong>EolaneVR</strong> is a Virtual Reality training system for the feeder loading operation of Surface-mount technology(SMT) customized for <strong><a href="https://www.eolane.com/en/eolane-china">éolane CHINA</a></strong>.
+     </br>
+     As the project leader, I spearheaded the development process, made the design, coordinated task assignments, and conducted feedback reviews for better iteration.
+     </br>
+     </br>
+     Here is the demo video of the project:
+     </div>
+
+    <!-- Video Frame -->
+    <div class="paragraph center">
+        <video controls style="width:100%; max-width:1920px; height:auto;">
+            <source src="https://game.tsingloo.com/Videos/EolaneVR_Demo.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+    
     <div class="paragraph">
         Main features :
         <ul>
@@ -25,13 +52,13 @@ export default [
     </div>
 
     <div class="paragraph center">
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
+        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="EolaneVR Screenshot" />
+        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="EolaneVR Screenshot" />
     </div>
     `, "#23bd69", true),
     new ProjectData("project-2", "Singing Addict", "img/projects/project-2-icon.png", `
     <div class="paragraph">
-        <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
+        <strong>EolaneVR Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
     </div>
     <div class="paragraph center">
