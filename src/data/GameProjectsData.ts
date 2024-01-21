4d5f49e1cae1868a245523d661dc00382d2694e5
TsingLoo <xiaonan.pan19@student.xjtlu.edu.cn>
2024-01-22 01:53:53 +0800

    content update

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -29,6 +29,7 @@ export default [
         <li>Real-time tape simulation</li>
         <li>Full feeder loading process</li>
         <li>Voice guide</li>
+        <li>International support</li>
         </ul>
     </div>
 
@@ -37,9 +38,9 @@ export default [
         <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="EolaneVR Screenshot" />
     </div>
     `, "#23bd69", true),
-    new ProjectData("project-2", "Singing Addict", "img/projects/project-2-icon.png", `
+    new ProjectData("project-2", "Wild Perception", "https://images.tsingloo.com/ShareX/2024/01/msedge_6kr9fWtPmd.gif", `
     <div class="paragraph">
-        <strong>EolaneVR Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
+        <strong>Wild Perception</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
     </div>
     <div class="paragraph center">
@@ -70,7 +71,7 @@ export default [
     <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
     </div>
     `, "#5a78af"),
-    new ProjectData("project-3", "Drawing Overload", "img/projects/project-3-icon.png", `
+    new ProjectData("project-3", "Verdant Revival", "img/projects/project-3-icon.png", `
     <div class="paragraph">
         <strong>Drawing Overload</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.
@@ -103,7 +104,7 @@ export default [
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
     </div>
     `, "#383838"),
-    new ProjectData("project-4", "Eugeneable", "img/projects/project-4-icon.png", `
+    new ProjectData("project-4", "ABB Digital Twin", "img/projects/project-4-icon.png", `
     <div class="paragraph">
     <strong>Eugeneable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
     <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@neo8iam">NEOSiAM 2020</a>.
@@ -130,7 +131,34 @@ export default [
         <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
     </div>
     `, "#e80fb7"),
-    new ProjectData("project-5", "Cloud Drew Land", "img/projects/project-5-icon.png", `
+    new ProjectData("project-5", "RTS Demo", "img/projects/project-4-icon.png", `
+    <div class="paragraph">
+    <strong>Eugeneable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
+    <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@neo8iam">NEOSiAM 2020</a>.
+    </div>
+    
+    <div class="paragraph">
+        Main features :
+        <ul>
+        <li>Some stuff</li>
+        <li>Some great stuff</li>
+        <li>More awesome stuff</li>
+        <li>And then some</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+        </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
+        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
+    </div>
+    `, "#e80fb7"),
+    new ProjectData("project-5", "Virtual Assembly", "img/projects/project-5-icon.png", `
     <div class="paragraph">
         <strong>Cloud Drew Land</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@cottonbro">cottonbro</a>.
